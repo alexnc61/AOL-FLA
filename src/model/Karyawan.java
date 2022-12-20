7b06400811d57696e4f41e35a6cea4895ca6fa6a
alexnc61 <alexsantoso278@gmail.com>
2022-12-20 17:36:59 +0700

    nambah fitur add karyawan

---
+++ src/model/Karyawan.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Karyawan {
+	private int id;
+	private String name;
+	private String date;
+	
+	public Karyawan(int id, String name, String date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.date = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+
+}

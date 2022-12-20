517274b205d1f5823030761a8f2fe0805c2aaf69
alexnc61 <alexsantoso278@gmail.com>
2022-12-20 16:29:04 +0700

    fitur menu doang

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+	
+	public void menu() {
+		System.out.println("1. Absen Karyawan");
+		System.out.println("2. Login Admin");
+	}
+	
+	public Main() {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Main();
+	}
+
+}
